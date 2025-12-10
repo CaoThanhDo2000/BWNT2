bf6ad67b7c166438be625d53158dc109ea7e005a
caothanhdo2000 <caothanhdo2000@gmail.com>
2025-12-10 09:11:42 +0000

    FIX CODE

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 // Thay thế 2 dòng dưới bằng thông tin bạn vừa copy bên Supabase
-const supabaseUrl = 'https://oleaxfpwarhhllmomxlt.supabase.co'
-const supabaseKey = 'sb_publishable_6Pn9rajNCmp5t_nf-XUP9Q_F8ghirjK'
+const supabaseUrl = "https://oleaxfpwarhhllmomxlt.supabase.co";
+const supabaseKey = "sb_publishable_6Pn9rajNCmp5t_nf-XUP9Q_F8ghirjK";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
